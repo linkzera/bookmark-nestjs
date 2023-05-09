3a8f3bde5360071dff52fc06dac69ccf07dea3c0
Douglas Vinicius <douglas04041996@hotmail.com>
2023-05-09 13:09:33 -0300

    feat: jwt

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,15 +1,24 @@
-import { Controller, Get, Req, UseGuards } from '@nestjs/common';
+import { Body, Controller, Get, Patch, UseGuards } from '@nestjs/common';
 import { UserService } from './user.service';
-import { Request } from 'express';
 import { JwtGuard } from 'src/auth/guard';
+import { GetUser } from 'src/auth/decorator';
 
+@UseGuards(JwtGuard)
 @Controller('users')
 export class UserController {
   constructor(private userService: UserService) {}
 
-  @UseGuards(JwtGuard)
   @Get('me')
-  getMe(@Req() req: Request) {
-    return this.userService.getMe(req.user);
+  getMe(@GetUser('id') userId: number) {
+    return this.userService.getMe(userId);
+  }
+
+  @Patch('me')
+  editUser(
+    @GetUser('id') userId: number,
+    @Body('firstName') firstName: string,
+    @Body('lastName') lastName: string,
+  ) {
+    return this.userService.editUser(firstName, lastName, userId);
   }
 }
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,8 +1,31 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
 
 @Injectable()
 export class UserService {
-  getMe(user) {
+  constructor(private prisma: PrismaService) {}
+
+  async getMe(userId: number) {
+    const user = await this.prisma.user.findUnique({
+      where: { id: userId },
+    });
+
+    delete user.hash;
+
+    return user;
+  }
+
+  async editUser(firstName: string, lastName: string, userId: number) {
+    const user = await this.prisma.user.update({
+      where: { id: userId },
+      data: {
+        firstName,
+        lastName,
+      },
+    });
+
+    delete user.hash;
+
     return user;
   }
 }
